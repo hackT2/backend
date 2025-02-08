e8e93b4cf7aa3237da75feedb04eec7ab872ae5a
임동재 <djlim1998@gmail.com>
2025-02-09 06:49:36 +0900

    #41 fix:stt Api 수정

--- src/main/java/hackathon/dragon/controller/SttRestController.java
+++ src/main/java/hackathon/dragon/controller/SttRestController.java
@@ -1,32 +1,38 @@
 package hackathon.dragon.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import hackathon.dragon.apipayLoad.ApiResponse;
 import hackathon.dragon.service.SpeechToTextService;
+import hackathon.dragon.service.toneChangeService.ToneChangeService;
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/stt")
 public class SttRestController {
 
     @Autowired
     private SpeechToTextService sttService;
+
+    private final ToneChangeService toneChangeService;
     /**
      * 오디오 파일을 받아서 텍스트로 변환하여 반환
      */
-    @PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "음성파일 텍스트로 변환", description = "텍스트반환")
-    public ApiResponse<String> handleAudioMessage(@RequestParam("audioFile") MultipartFile audioFile) throws IOException {
+    public ApiResponse<List<String>> handleAudioMessage(
+            @PathVariable Long id,
+            @RequestParam("audioFile") MultipartFile audioFile) throws IOException {
         String transcribe = sttService.transcribe(audioFile);
-        return ApiResponse.onSuccess(transcribe);
+        List<String> responseLines = toneChangeService.requestTextAnalysis(transcribe, id);
+        return ApiResponse.onSuccess(responseLines);
     }
 
 }
