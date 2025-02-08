4a2bfbdfc72a0a716ea314613f7eac3c9d4df740
임동재 <djlim1998@gmail.com>
2025-02-09 02:35:46 +0900

    #29 feat: tone 정보반환 api

---
+++ src/main/java/hackathon/dragon/dto/ToneDto/response/TonePromptResponseDto.java
@@ -0,0 +1,16 @@
+package hackathon.dragon.dto.ToneDto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TonePromptResponseDto {
+    private Long id;
+    private String name;
+    private String explanation;
+    private String example;
+    private String prompt;
+}
--- src/main/java/hackathon/dragon/service/toneService/ToneService.java
+++ src/main/java/hackathon/dragon/service/toneService/ToneService.java
@@ -1,6 +1,7 @@
 package hackathon.dragon.service.toneService;
 
 import hackathon.dragon.dto.ToneDto.request.ToneRequestDto;
+import hackathon.dragon.dto.ToneDto.response.TonePromptResponseDto;
 import hackathon.dragon.dto.ToneDto.response.ToneResponseDto;
 
 import java.util.List;
@@ -10,6 +11,8 @@ public interface ToneService {
     List<ToneResponseDto> getAllTones();
 
     ToneResponseDto createTone(ToneRequestDto toneRequestDto);
+
+    TonePromptResponseDto getTonePrompt(Long toneId);
 
     void deleteTone(Long toneId);
 }
